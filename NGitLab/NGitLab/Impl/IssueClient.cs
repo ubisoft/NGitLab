1e7a5de7f335c62d6c356dfdb82fb0a294f0cd4f
petro.margaritis <petro.margaritis@surgeons.org>
2014-11-03 13:17:44 +1100

    added IssueClient with method to retrieve authenticated users owned issues, and a ForProject method to retreive a projects issues.

---
+++ NGitLab/NGitLab/Impl/IssueClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using NGitLab.Models;
+
+namespace NGitLab.Impl
+{
+    public class IssueClient : IIssueClient
+    {
+        private const string IssuesUrl = "/issues";
+        private const string ProjectIssuesUrl = "/projects/{0}/issues";
+        private readonly API _api;
+
+        public IssueClient(API api)
+        {
+            _api = api;
+        }
+
+        public IEnumerable<Issue> Owned
+        {
+            get
+            {
+                return _api.Get().GetAll<Issue>(IssuesUrl);
+            }
+        }
+
+        public IEnumerable<Issue> ForProject(int projectId)
+        {
+            return _api.Get().GetAll<Issue>(string.Format(ProjectIssuesUrl, projectId));
+        }
+    }
+}
---
+++ NGitLab/NGitLab/Models/Milestone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NGitLab.Models
+{
+    [DataContract]
+    public class Milestone
+    {
+        [DataMember(Name = "id")]
+        public int Id;
+
+        [DataMember(Name = "title")]
+        public string Title;
+
+        [DataMember(Name = "description")]
+        public string Description;
+
+        [DataMember(Name = "due_date")]
+        public string DueDate;
+
+        [DataMember(Name = "state")]
+        public string State;
+
+        [DataMember(Name = "created_at")]
+        public DateTime CreatedAt;
+
+        [DataMember(Name = "updated_at")]
+        public DateTime UpdatedAt;
+    }
+}
