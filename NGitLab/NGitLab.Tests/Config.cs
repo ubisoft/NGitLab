1f871b176f7b966b119e2f7ee9a3d192596bb5b7
Maxim Litvinov <maximatorr@bk.ru>
2017-08-23 10:08:44 +0300

    add config

--- NGitLab/NGitLab.Tests/Config.cs
+++ NGitLab/NGitLab.Tests/Config.cs
@@ -1,7 +1,7 @@
 ﻿namespace NGitLab.Tests {
     public static class Config {
-        public const string ServiceUrl = "https://gitlab.com/api/v3";
-        public const string Secret = "TOKEN";
+        public const string ServiceUrl = "http://gitserver/";
+        public const string Secret = "kpdcucE1Y4wykmqBGD4x";
 
         public static GitLabClient Connect() {
             return GitLabClient.Connect(ServiceUrl, Secret);
