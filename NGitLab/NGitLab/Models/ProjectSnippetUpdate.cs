ac9a8f56e78b2cf194fdb8bf1c14b3332e9071d1
MysticBoy <mysticboy@live.com>
2017-10-09 01:10:28 +0800

    加入ProjectSnippetUpdate.cs

---
+++ NGitLab/NGitLab/Models/ProjectSnippetUpdate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NGitLab.Models
+{
+    [DataContract]
+    public class ProjectSnippetUpdate : ProjectSnippetInsert
+    {
+        [DataMember(Name = "snippet_id")]
+        public int SnippetID { get; set; }
+    }
+}
