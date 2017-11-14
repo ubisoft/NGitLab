195c06c7268d0f7399be24346b856cea77cbc627
gerald barre <gerald.barre@ubisoft.com>
2017-11-14 13:32:14 -0500

    Improve ProjectHook

--- NGitLab/NGitLab/Models/ProjectHookUpsert.cs
+++ NGitLab/NGitLab/Models/ProjectHookUpsert.cs
@@ -13,8 +13,29 @@ namespace NGitLab.Models
 
         [DataMember(Name = "push_events")]
         public bool PushEvents;
-        
+
         [DataMember(Name = "merge_requests_events")]
         public bool MergeRequestsEvents;
+
+        [DataMember(Name = "issues_events")]
+        public bool IssuesEvents;
+
+        [DataMember(Name = "tag_push_events")]
+        public bool TagPushEvents;
+
+        [DataMember(Name = "note_events")]
+        public bool NoteEvents;
+
+        [DataMember(Name = "job_events")]
+        public bool JobEvents;
+
+        [DataMember(Name = "pipeline_events")]
+        public bool PipelineEvents;
+        
+        [DataMember(Name = "enable_ssl_verification")]
+        public bool EnableSslVerification;
+
+        [DataMember(Name = "token")]
+        public string Token;
     }
 }
