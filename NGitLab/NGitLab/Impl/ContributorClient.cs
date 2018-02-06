4b4060d62794984281cef71cf1dd0a778d7f5d67
Unknown <charles.choiniere-denicourt@ubisoft.com>
2018-02-06 11:11:18 -0500

    Added Git lab contributors

---
+++ NGitLab/NGitLab/Impl/ContributorClient.cs
@@ -0,0 +1,22 @@
+﻿using NGitLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGitLab.Impl
+{
+    class ContributorClient: IContributorClient
+    {
+        private readonly API _api;
+        private readonly string _contributorPath;
+
+        public ContributorClient(API api, string repoPath)
+        {
+            _api = api;
+            _contributorPath = repoPath +Contributor.Url;
+        }
+        public IEnumerable<Contributor> All => _api.Get().GetAll<Contributor>(_contributorPath);
+    }
+}
---
+++ NGitLab/NGitLab/Models/Contributor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGitLab.Models
+{
+    
+    [DataContract]
+    public class Contributor
+    {
+        public const string Url = "/contributors";
+
+        [DataMember(Name = "name")]
+        public string Name;
+
+        [DataMember(Name = "email")]
+        public string Email;
+
+        [DataMember(Name = "commits")]
+        public int Commits;
+
+        [DataMember(Name = "additions")]
+        public int Addition;
+
+        [DataMember(Name = "deletions")]
+        public int Deletions;
+    }
+}
