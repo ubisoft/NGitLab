d215ab6ca0a43c2341f4c175c246337659779ed6
Thomas Cortès <thomas.cortes@ubisoft.com>
2018-03-08 21:28:52 +0000

    map tree/blob to comply with current gitlab api structure

---
+++ NGitLab/NGitLab/Models/Blob.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace NGitLab.Models
+{
+    [DataContract]
+    public class Blob
+    {
+        [DataMember(Name = "size")]
+        public int Size;
+
+        [DataMember(Name = "encoding")]
+        public string Encoding;
+
+        [DataMember(Name = "content")]
+        public string Content;
+
+        [DataMember(Name = "sha")]
+        public Sha1 Sha;
+    }
+}
