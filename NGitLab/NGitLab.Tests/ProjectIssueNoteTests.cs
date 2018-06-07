fc1cbd54d49a2d6da983c2b5b508fd462c0688e0
Luc Poirier <luc.poirier@ubisoft.com>
2018-06-07 15:05:53 -0400

    - Split the unit test in several tests for better granularity - Add unit for Get() method

--- NGitLab/NGitLab.Tests/ProjectIssueNoteTests.cs
+++ NGitLab/NGitLab.Tests/ProjectIssueNoteTests.cs
@@ -17,7 +17,7 @@ namespace NGitLab.Tests
         }
         
         [Test]
-        public void CreateListEdit()
+        public void CreateNote()
         {
             var noteCreate = new ProjectIssueNoteCreate()
             {
@@ -28,11 +28,63 @@ namespace NGitLab.Tests
             var createdNote = _client.Create(noteCreate);
 
             Assert.That(createdNote.Body, Is.EqualTo(noteCreate.Body));
+        }
 
-            var listedNote = _client.ForIssue(Initialize.UnitTestIssue.IssueId).ToList();
-            Assert.That(listedNote.Count, Is.EqualTo(1));
+        [Test]
+        public void ListNotes()
+        {
+            var createdIssue = Initialize.GitLabClient.Issues.Create(new IssueCreate()
+            {
+                Id = Initialize.UnitTestProject.Id,
+                Title = "title",
+                Description = "desc",
+            });
+
+            var noteCreate = new ProjectIssueNoteCreate()
+            {
+                IssueId = createdIssue.IssueId,
+                Body = "Issue Body"
+            };
+
+            _client.Create(noteCreate);
+            _client.Create(noteCreate);
+
+            var listedNote = _client.ForIssue(createdIssue.IssueId).ToList();
+            // Gitlab create additional issue notes about initial manipulation on the issue,
+            // so a starting count greater than one is expected.  Those issue notes have the 'system'
+            // boolean set to true.
+            // We validate that there is only two issues that don't have the 'system' flag.
+            Assert.That(listedNote.Where(x => !x.System).Count, Is.EqualTo(2));
             Assert.That(listedNote.First().Body, Is.EqualTo(noteCreate.Body));
+        }
 
+        [Test]
+        public void GetNote()
+        {
+            var noteCreate = new ProjectIssueNoteCreate()
+            {
+                IssueId = Initialize.UnitTestIssue.IssueId,
+                Body = "Issue Body"
+            };
+
+            var createdNote = _client.Create(noteCreate);
+
+            var getNote = _client.Get(Initialize.UnitTestIssue.IssueId, createdNote.NoteId);
+            Assert.That(getNote.Body, Is.EqualTo(noteCreate.Body));
+        }
+
+        [Test]
+        public void EditNote()
+        {
+            var noteCreate = new ProjectIssueNoteCreate()
+            {
+                IssueId = Initialize.UnitTestIssue.IssueId,
+                Body = "Issue Body"
+            };
+
+            var createdNote = _client.Create(noteCreate);
+
+            Assert.That(createdNote.Body, Is.EqualTo(noteCreate.Body));
 
             var noteEdit = new ProjectIssueNoteEdit()
             {
