95c6677bad3ff5e076f69f06239089c80c7df656
Jonathan T-Delorme <jonathan.t-delorme@ubisoft.com>
2018-08-27 14:57:48 -0400

    Fix flaky "job" related tests
    - Stop to test the deprecated api
    - Increment the timeout to 30 seconds

--- NGitLab/NGitLab.Tests/PipelineTests.cs
+++ NGitLab/NGitLab.Tests/PipelineTests.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading;
 using NGitLab.Models;
 using NUnit.Framework;
 
 namespace NGitLab.Tests
 {
+    [Timeout(30000)]
     public class PipelineTests
     {
         private IPipelineClient _pipelines;
@@ -13,89 +16,44 @@ namespace NGitLab.Tests
         {
             _pipelines = Initialize.GitLabClient.GetPipelines(Initialize.UnitTestProject.Id);
             CommitsTests.EnableCiOnTestProject();
+
+            CreatePipeline();
         }
 
-        [Test, Timeout(10000)]
+        private void CreatePipeline()
+        {
+            Initialize.GitLabClient.GetRepository(Initialize.UnitTestProject.Id).Tags.Create(new TagCreate
+            {
+                Name = "NewTagForPipelineTests",
+                Ref = "master"
+            });
+
+            while (FindPipeline("NewTagForPipelineTests") == null)
+            { 
+                Console.WriteLine("Waiting for pipeline to start.");
+                Thread.Sleep(1000);
+            }
+        }
+        
+        [Test]
         public void Test_can_list_the_pipeline_of_the_current_tag()
         {
-            Initialize.GitLabClient.GetRepository(Initialize.UnitTestProject.Id).Tags.Create(new TagCreate
-            {
-                Name = "NewTagForBuild",
-                Ref = "master"
-            });
+            PipelineBasic thisTagPipeline = FindPipeline("NewTagForPipelineTests");
 
-            PipelineBasic thisTagPipeline;
-
-            while (true)
-            {
-                var pipelines = _pipelines.All.ToArray();
-                thisTagPipeline = pipelines.FirstOrDefault(x => x.Ref == "NewTagForBuild");
-
-                if (thisTagPipeline != null && _pipelines[thisTagPipeline.Id].Ref == "NewTagForBuild")
-                {
-                    break;
-                }
-            }
-
-            while (true)
-            {
-                var jobs = _pipelines.GetJobs(thisTagPipeline.Id);
-
-                if (jobs.Length > 0)
-                    return;
-            }
+            Assert.IsNotNull(thisTagPipeline);
         }
 
-        [Test, Timeout(10000)]
-        public void Test_can_list_pipelines_with_scope_all()
-        {
-            Initialize.GitLabClient.GetRepository(Initialize.UnitTestProject.Id).Tags.Create(new TagCreate
-            {
-                Name = "NewTagForBuildScopeAll",
-                Ref = "master"
-            });
-
-            while (true)
-            {
-#pragma warning disable 618 // Obsolete
-                if (_pipelines.GetJobsInProject(JobScope.All).Any())
-                    return;
-#pragma warning restore 618
-            }
-        }
-
-        [Test, Timeout(10000)]
-        public void Test_can_list_pipelines_with_scope_pending()
-        {
-            Initialize.GitLabClient.GetRepository(Initialize.UnitTestProject.Id).Tags.Create(new TagCreate
-            {
-                Name = "NewTagForBuildScopePending",
-                Ref = "master"
-            });
-
-            while (true)
-            {
-#pragma warning disable 618 // Obsolete
-                if (_pipelines.GetJobsInProject(JobScope.Pending).Any())
-                    return;
-#pragma warning restore 618
-            }
-        }
-
-        [Test, Timeout(10000)]
+        [Test]
         public void Test_can_list_all_jobs_from_project()
         {
-            Initialize.GitLabClient.GetRepository(Initialize.UnitTestProject.Id).Tags.Create(new TagCreate
-            {
-                Name = "NewTagForBuildAllJobs",
-                Ref = "master"
-            });
+            var allJobs = _pipelines.AllJobs;
 
-            while (true)
-            {
-                if (_pipelines.AllJobs.Any())
-                    return;
-            }
+            Assert.That(allJobs.Any());
+        }
+
+        private PipelineBasic FindPipeline(string refName)
+        {
+            return _pipelines.All.FirstOrDefault(x => x.Ref == refName);
         }
     }
 }
