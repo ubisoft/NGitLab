001c078857a254febd63bb1311ac64eb69109643
Jonathan T-Delorme <jonathan.t-delorme@ubisoft.com>
2018-08-27 13:02:08 -0400

    Expose the http timeout in ngitlab to allow easy configuration
    GitLab exposes calls that are really slow. Our internal GitLab instance is now configured with an internal timeout of 5 minutes. Increment the default timeout the the same value.

--- NGitLab/NGitLab.Tests/HttpRequestorTests.cs
+++ NGitLab/NGitLab.Tests/HttpRequestorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using NGitLab.Impl;
 using NUnit.Framework;
@@ -11,7 +12,7 @@ namespace NGitLab.Tests
         [Test]
         public void Test_calls_are_retried_when_they_fail_in_gitlab()
         {
-            var requestOptions = new MockRetryer(1, TimeSpan.FromMilliseconds(10), isIncremental: false);
+            var requestOptions = new MockRequestOptions(1, TimeSpan.FromMilliseconds(10), isIncremental: false);
             var httpRequestor = new HttpRequestor(Initialize.GitLabHost, Initialize.GitLabToken, MethodType.Get, requestOptions);
 
             Assert.Throws<GitLabException>(() => httpRequestor.Execute("invalidUrl"));
@@ -19,13 +20,29 @@ namespace NGitLab.Tests
             Assert.That(requestOptions.HandledRequests.Count, Is.EqualTo(2));
         }
 
-        private class MockRetryer : RequestOptions
+        [Test]
+        public void Test_the_timeout_can_be_overridden_in_the_request_options()
+        {
+            var requestOptions = new MockRequestOptions { HttpClientTimeout = TimeSpan.FromMinutes(2) };
+
+            var httpRequestor = new HttpRequestor(Initialize.GitLabHost, Initialize.GitLabToken, MethodType.Get, requestOptions);
+            Assert.Throws<GitLabException>(() => httpRequestor.Execute("invalidUrl"));
+
+            Assert.AreEqual(TimeSpan.FromMinutes(2).TotalMilliseconds, requestOptions.HandledRequests.Single().Timeout);
+        }
+
+        private class MockRequestOptions : RequestOptions
         {
             public bool ShouldRetryCalled { get; set; }
 
             public HashSet<WebRequest> HandledRequests { get; } = new HashSet<WebRequest>();
 
-            public MockRetryer(int retryCount, TimeSpan retryInterval, bool isIncremental = true)
+            public MockRequestOptions()
+                : base(retryCount: 0, retryInterval: TimeSpan.Zero)
+            {
+            }
+
+            public MockRequestOptions(int retryCount, TimeSpan retryInterval, bool isIncremental = true)
                 : base(retryCount, retryInterval, isIncremental)
             {
             }
@@ -40,7 +57,7 @@ namespace NGitLab.Tests
             public override WebResponse GetResponse(HttpWebRequest request)
             {
                 HandledRequests.Add(request);
-                throw new GitLabException() { StatusCode = HttpStatusCode.InternalServerError };
+                throw new GitLabException { StatusCode = HttpStatusCode.InternalServerError };
             }
         }
     }
