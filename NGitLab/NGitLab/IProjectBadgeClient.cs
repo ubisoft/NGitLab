eb70545921a69c66c8b2d1d1e29347f78933117d
Pascal Laurin <pascal.laurin@ubisoft.com>
2018-11-07 14:38:20 +0000

    Cleanup badges properly before tests

--- NGitLab/NGitLab/IProjectBadgeClient.cs
+++ NGitLab/NGitLab/IProjectBadgeClient.cs
@@ -6,6 +6,12 @@ namespace NGitLab
     public interface IProjectBadgeClient
     {
         IEnumerable<Badge> All { get; }
+
+        /// <summary>
+        /// Gets the project's badges only (not group badges)
+        /// </summary>
+        /// <remarks>Project Badge API returns both Group and Project badges</remarks>
+        IEnumerable<Badge> ProjectsOnly { get; }
         Badge this[int id] { get; }
         Badge Create(BadgeCreate badge);
         Badge Update(int id, BadgeUpdate badge);
