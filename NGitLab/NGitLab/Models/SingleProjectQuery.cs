fa0348f7468f4de04ca3520cd3ab2ffcc50f4478
Marc-André Montreuil <marc-andre.montreuil@ubisoft.com>
2018-12-19 15:43:18 -0500

    Add a Get by id query

---
+++ NGitLab/NGitLab/Models/SingleProjectQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NGitLab.Models
+{
+    /// <summary>
+    /// Allows to use more advanced gitlab queries for getting projects.
+    /// </summary>
+    [DataContract]
+    public class SingleProjectQuery
+    {
+        /// <summary>
+        /// Include project statistics
+        /// </summary>
+        public bool? Statistics;
+    }
+}
