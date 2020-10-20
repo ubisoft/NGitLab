0f1470408d0c76cf269defcc93c06117ee170242
Philippe Fortin <philippe.fortin2@ubisoft.com>
2020-10-20 09:36:05 -0400

    fix deployment query params to match api

--- NGitLab/Impl/DeploymentClient.cs
+++ NGitLab/Impl/DeploymentClient.cs
@@ -28,12 +28,10 @@ namespace NGitLab.Impl
 
         private IEnumerable<Deployment> Get(string url, DeploymentQuery query)
         {
-            url = Utils.AddParameter(url, "state", query.Status);
+            url = Utils.AddParameter(url, "status", query.Status);
             url = Utils.AddParameter(url, "order_by", query.OrderBy);
             url = Utils.AddParameter(url, "sort", query.Sort);
             url = Utils.AddParameter(url, "environment", query.Environment);
-            url = Utils.AddParameter(url, "created_after", query.CreatedAfter);
-            url = Utils.AddParameter(url, "created_before", query.CreatedBefore);
             url = Utils.AddParameter(url, "updated_after", query.UpdatedAfter);
             url = Utils.AddParameter(url, "updated_before", query.UpdatedBefore);
 
