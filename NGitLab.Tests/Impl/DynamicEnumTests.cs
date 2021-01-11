28c20cb407143c24bbb5128e0c9d5cace220f510
Gérald Barré <gerald.barre@ubisoft.com>
2021-01-11 11:27:31 -0500

    Fix CA1812 warnings

--- NGitLab.Tests/Impl/DynamicEnumTests.cs
+++ NGitLab.Tests/Impl/DynamicEnumTests.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace NGitLab.Tests.Impl
 {
-    class DynamicEnumTests
+    public class DynamicEnumTests
     {
         [Test]
         public void Test_comparison()
