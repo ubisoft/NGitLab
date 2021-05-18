d3c673ea56edb1ec5e3adca6b77326108548cf68
Louis Zanella <louis.zanella@ubisoft.com>
2021-05-18 19:48:23 -0400

    Use TestContext.Out instead of TestContext.Progress

--- NGitLab.Tests/Docker/GitLabTestContext.cs
+++ NGitLab.Tests/Docker/GitLabTestContext.cs
@@ -23,7 +23,7 @@ namespace NGitLab.Tests.Docker
         private static readonly Policy s_gitlabRetryPolicy = Policy.Handle<GitLabException>()
             .WaitAndRetry(_maxRetryCount, _ => TimeSpan.FromSeconds(1), (exception, timespan, retryCount, context) =>
             {
-                TestContext.Progress.WriteLine($"{exception.Message} -> Polly Retry {retryCount} of {_maxRetryCount}...");
+                TestContext.Out.WriteLine($"{exception.Message} -> Polly Retry {retryCount} of {_maxRetryCount}...");
             });
 
         private static readonly HashSet<string> s_generatedValues = new HashSet<string>(StringComparer.Ordinal);
@@ -387,7 +387,7 @@ namespace NGitLab.Tests.Docker
             while (!predicate(result))
             {
                 cancellationToken.ThrowIfCancellationRequested();
-                TestContext.Progress.WriteLine($"RetryUntilAsync {retryCount++}...");
+                TestContext.Out.WriteLine($"RetryUntilAsync {retryCount++}...");
                 await Task.Delay(1000, cancellationToken).ConfigureAwait(false);
 
                 result = action();
