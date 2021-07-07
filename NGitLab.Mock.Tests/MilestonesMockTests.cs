2945c518f309f41991cf1bf1b678ffcb222069e2
Arthur Ferré <arthur.ferre@ubisoft.com>
2021-07-07 18:05:22 +0200

    Implement milestone client for mocks

---
+++ NGitLab.Mock.Tests/MilestonesMockTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using NGitLab.Models;
+using NUnit.Framework;
+
+namespace NGitLab.Mock.Tests
+{
+    public class MilestonesMockTests
+    {
+        [Test]
+        public void Test_milestones_from_project_can_be_found()
+        {
+            using var gitLabServer = new GitLabServer();
+            var user1 = new User("user1");
+            gitLabServer.Users.Add(user1);
+            var group = new Group("TestGroup");
+            gitLabServer.Groups.Add(group);
+            var project = new Project("Test") { Visibility = VisibilityLevel.Internal };
+            group.Projects.Add(project);
+            project.Milestones.Add(new Milestone { Title = "Milestone 1" });
+            project.Milestones.Add(new Milestone { Title = "Milestone 2" });
+
+            var client = gitLabServer.CreateClient(user1);
+            var milestones = client.GetMilestone(project.Id).All.ToArray();
+
+            Assert.AreEqual(2, milestones.Length, "Milestones count is invalid");
+            Assert.IsTrue(milestones.Any(x => string.Equals(x.Title, "Milestone 1", StringComparison.Ordinal)), "Milestone 'Milestone 1' not found");
+            Assert.IsTrue(milestones.Any(x => string.Equals(x.Title, "Milestone 2", StringComparison.Ordinal)), "Milestone 'Milestone 2' not found");
+        }
+
+        [Test]
+        public void Test_milestones_can_be_added_to_project()
+        {
+            using var gitLabServer = new GitLabServer();
+            var user1 = new User("user1");
+            gitLabServer.Users.Add(user1);
+            var group = new Group("TestGroup");
+            gitLabServer.Groups.Add(group);
+            var project = new Project("Test") { Visibility = VisibilityLevel.Internal };
+            group.Projects.Add(project);
+
+            var client = gitLabServer.CreateClient(user1);
+            client.GetMilestone(project.Id).Create(new MilestoneCreate { Title = "Milestone 1" });
+            var milestones = client.GetMilestone(project.Id).All.ToArray();
+
+            Assert.AreEqual(1, milestones.Length, "Milestones count is invalid");
+            Assert.AreEqual("Milestone 1", milestones[0].Title, "Milestone 'Milestone 1' not found");
+        }
+
+        [Test]
+        public void Test_milestones_can_be_edited_from_project()
+        {
+            using var gitLabServer = new GitLabServer();
+            var user1 = new User("user1");
+            gitLabServer.Users.Add(user1);
+            var group = new Group("TestGroup");
+            gitLabServer.Groups.Add(group);
+            var project = new Project("Test") { Visibility = VisibilityLevel.Internal };
+            group.Projects.Add(project);
+            var milestone = new Milestone { Title = "Milestone 1" };
+            project.Milestones.Add(milestone);
+
+            var client = gitLabServer.CreateClient(user1);
+            client.GetMilestone(project.Id).Update(milestone.Id, new MilestoneUpdate { Title = "Milestone 2" });
+            var milestones = client.GetMilestone(project.Id).All.ToArray();
+
+            Assert.AreEqual(1, milestones.Length, "Milestones count is invalid");
+            Assert.AreEqual("Milestone 2", milestones[0].Title, "Milestone 'Milestone 2' not found");
+        }
+
+        [Test]
+        public void Test_milestones_can_be_deleted_from_project()
+        {
+            using var gitLabServer = new GitLabServer();
+            var user1 = new User("user1");
+            gitLabServer.Users.Add(user1);
+            var group = new Group("TestGroup");
+            gitLabServer.Groups.Add(group);
+            var project = new Project("Test") { Visibility = VisibilityLevel.Internal };
+            group.Projects.Add(project);
+            var milestone = new Milestone { Title = "Milestone 1" };
+            project.Milestones.Add(milestone);
+
+            var client = gitLabServer.CreateClient(user1);
+            client.GetMilestone(project.Id).Delete(milestone.Id);
+            var milestones = client.GetMilestone(project.Id).All.ToArray();
+
+            Assert.AreEqual(0, milestones.Length, "Milestones count is invalid");
+        }
+
+        [Test]
+        public void Test_milestones_can_be_closed_and_activated_from_project()
+        {
+            using var gitLabServer = new GitLabServer();
+            var user1 = new User("user1");
+            gitLabServer.Users.Add(user1);
+            var group = new Group("TestGroup");
+            gitLabServer.Groups.Add(group);
+            var project = new Project("Test") { Visibility = VisibilityLevel.Internal };
+            group.Projects.Add(project);
+            var milestone = new Milestone { Title = "Milestone 1" };
+            project.Milestones.Add(milestone);
+
+            var client = gitLabServer.CreateClient(user1);
+            client.GetMilestone(project.Id).Close(milestone.Id);
+            var activeMilestones = client.GetMilestone(project.Id).AllInState(Models.MilestoneState.active).ToArray();
+            var closedMilestones = client.GetMilestone(project.Id).AllInState(Models.MilestoneState.closed).ToArray();
+
+            Assert.AreEqual(0, activeMilestones.Length, "Active milestones count is invalid");
+            Assert.AreEqual(1, closedMilestones.Length, "Closed milestones count is invalid");
+
+            client.GetMilestone(project.Id).Activate(milestone.Id);
+            activeMilestones = client.GetMilestone(project.Id).AllInState(Models.MilestoneState.active).ToArray();
+            closedMilestones = client.GetMilestone(project.Id).AllInState(Models.MilestoneState.closed).ToArray();
+
+            Assert.AreEqual(1, activeMilestones.Length, "Active milestones count is invalid");
+            Assert.AreEqual(0, closedMilestones.Length, "Closed milestones count is invalid");
+        }
+    }
+}
--- NGitLab.Mock/Clients/MilestoneClient.cs
+++ NGitLab.Mock/Clients/MilestoneClient.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NGitLab.Mock.Clients;
 using NGitLab.Models;
 
@@ -14,43 +16,119 @@ namespace NGitLab.Mock
             _projectId = projectId;
         }
 
-        public Models.Milestone this[int id] => throw new System.NotImplementedException();
+        public Models.Milestone this[int id]
+        {
+            get
+            {
+                var project = FindProject(_projectId) ?? throw new InvalidOperationException($"Cannot find project with ID {_projectId}");
+                return FindMilestone(id, project)?.ToClientMilestone();
+            }
+        }
 
-        public IEnumerable<Models.Milestone> All => throw new System.NotImplementedException();
+        public IEnumerable<Models.Milestone> All
+        {
+            get
+            {
+                var project = FindProject(_projectId) ?? throw new InvalidOperationException($"Cannot find project with ID {_projectId}");
+                return project.Milestones.Select(x => x.ToClientMilestone());
+            }
+        }
 
         public Models.Milestone Activate(int milestoneId)
         {
-            throw new System.NotImplementedException();
+            var project = FindProject(_projectId) ?? throw new InvalidOperationException($"Cannot find project with ID {_projectId}");
+            var milestone = FindMilestone(milestoneId, project) ?? throw new InvalidOperationException($"Cannot find milestone with ID {milestoneId}");
+            milestone.State = MilestoneState.active;
+            return milestone.ToClientMilestone();
         }
 
         public IEnumerable<Models.Milestone> AllInState(Models.MilestoneState state)
         {
-            throw new System.NotImplementedException();
+            var stateAsString = state.ToString();
+            return All.Where(x => string.Equals(x.State, stateAsString, StringComparison.Ordinal));
         }
 
         public Models.Milestone Close(int milestoneId)
         {
-            throw new System.NotImplementedException();
+            var project = FindProject(_projectId) ?? throw new InvalidOperationException($"Cannot find project with ID {_projectId}");
+            var milestone = FindMilestone(milestoneId, project) ?? throw new InvalidOperationException($"Cannot find milestone with ID {milestoneId}");
+            milestone.State = MilestoneState.closed;
+            return milestone.ToClientMilestone();
         }
 
         public Models.Milestone Create(MilestoneCreate milestone)
         {
-            throw new System.NotImplementedException();
+            var project = FindProject(_projectId) ?? throw new InvalidOperationException($"Cannot find project with ID {_projectId}");
+            var ms = new Milestone
+            {
+                Title = milestone.Title,
+                Description = milestone.Description,
+                DueDate = string.IsNullOrEmpty(milestone.DueDate) ? DateTimeOffset.Now : DateTimeOffset.Parse(milestone.DueDate),
+                StartDate = string.IsNullOrEmpty(milestone.StartDate) ? DateTimeOffset.Now : DateTimeOffset.Parse(milestone.StartDate),
+            };
+            project.Milestones.Add(ms);
+            return ms.ToClientMilestone();
         }
 
         public void Delete(int milestoneId)
         {
-            throw new System.NotImplementedException();
+            var project = FindProject(_projectId) ?? throw new InvalidOperationException($"Cannot find project with ID {_projectId}");
+            var milestone = FindMilestone(milestoneId, project) ?? throw new InvalidOperationException($"Cannot find milestone with ID {milestoneId}");
+            project.Milestones.Remove(milestone);
         }
 
         public IEnumerable<Models.Milestone> Get(MilestoneQuery query)
         {
-            throw new System.NotImplementedException();
+            var project = FindProject(_projectId) ?? throw new InvalidOperationException($"Cannot find project with ID {_projectId}");
+            IEnumerable<Milestone> milestones = project.Milestones;
+            if (query.State != null)
+            {
+                milestones = milestones.Where(m => (int)m.State == (int)query.State);
+            }
+
+            if (!string.IsNullOrEmpty(query.Search))
+            {
+                milestones = milestones.Where(m => m.Title.Contains(query.Search, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return milestones.Select(m => m.ToClientMilestone());
         }
 
         public Models.Milestone Update(int milestoneId, MilestoneUpdate milestone)
         {
-            throw new System.NotImplementedException();
+            var project = FindProject(_projectId) ?? throw new InvalidOperationException($"Cannot find project with ID {_projectId}");
+            var ms = FindMilestone(milestoneId, project) ?? throw new InvalidOperationException($"Cannot find milestone with ID {milestoneId}");
+            if (!string.IsNullOrEmpty(milestone.Title))
+            {
+                ms.Title = milestone.Title;
+            }
+
+            if (milestone.Description != null)
+            {
+                ms.Description = milestone.Description;
+            }
+
+            if (!string.IsNullOrEmpty(milestone.DueDate))
+            {
+                ms.DueDate = DateTimeOffset.Parse(milestone.DueDate);
+            }
+
+            if (!string.IsNullOrEmpty(milestone.StartDate))
+            {
+                ms.StartDate = DateTimeOffset.Parse(milestone.StartDate);
+            }
+
+            return ms.ToClientMilestone();
+        }
+
+        private Project FindProject(int id)
+        {
+            return Server.AllProjects.FindById(id);
+        }
+
+        private static Milestone FindMilestone(int id, Project project)
+        {
+            return project.Milestones.FirstOrDefault(x => x.Id == id);
         }
     }
 }
