edd997f76aac548a7a98124b1b3c1bb3ac0d0381
Thomas Cortes <Thomas.Cortes@ubisoft.com>
2021-07-29 15:50:46 -0400

    Add missing variable properties

--- NGitLab/Models/Variable.cs
+++ NGitLab/Models/Variable.cs
@@ -13,5 +13,14 @@ namespace NGitLab.Models
 
         [DataMember(Name = "protected")]
         public bool Protected;
+
+        [DataMember(Name = "variable_type")]
+        public VariableType Type;
+
+        [DataMember(Name = "masked")]
+        public bool Masked;
+
+        [DataMember(Name = "environment_scope")]
+        public string Scope;
     }
 }
---
+++ NGitLab/Models/VariableType.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace NGitLab.Models
+{
+    public enum VariableType
+    {
+        [EnumMember(Value = "env_var")]
+        Variable,
+        [EnumMember(Value = "file")]
+        File,
+    }
+}
