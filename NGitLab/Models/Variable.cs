933c232058d15d7c839a83efeabad45bc117dbb7
Thomas Cortes <Thomas.Cortes@ubisoft.com>
2021-07-30 09:30:54 -0400

    Add public api & transform variable to properties

--- NGitLab/Models/Variable.cs
+++ NGitLab/Models/Variable.cs
@@ -6,21 +6,21 @@ namespace NGitLab.Models
     public class Variable
     {
         [DataMember(Name = "key")]
-        public string Key;
+        public string Key { get; set; }
 
         [DataMember(Name = "value")]
-        public string Value;
+        public string Value { get; set; }
 
         [DataMember(Name = "protected")]
-        public bool Protected;
+        public bool Protected { get; set; }
 
         [DataMember(Name = "variable_type")]
-        public VariableType Type;
+        public VariableType Type { get; set; }
 
         [DataMember(Name = "masked")]
-        public bool Masked;
+        public bool Masked { get; set; }
 
         [DataMember(Name = "environment_scope")]
-        public string Scope;
+        public string Scope { get; set; }
     }
 }
