f640b892d8fa0738461b56174b884d2926e38b82
Dominick Latreille <dominick.latreille@ubisoft.com>
2021-10-19 16:04:27 -0400

    Implement Repository Tree in Mock

--- NGitLab.Mock/Repository.cs
+++ NGitLab.Mock/Repository.cs
@@ -440,6 +440,21 @@ namespace NGitLab.Mock
             };
         }
 
+        internal IEnumerable<Models.Tree> GetTree()
+        {
+            var repo = GetGitRepository();
+            Commands.Checkout(repo, Project.DefaultBranch);
+
+            foreach(var fileSystemEntry in Directory.EnumerateFileSystemEntries(FullPath))
+            {
+                yield return new Models.Tree
+                {
+                    Name = Path.GetFileName(fileSystemEntry),
+                    Path = Path.GetFileName(fileSystemEntry),
+                };
+            }
+        }
+
         public Commit Merge(User user, string sourceBranch, string targetBranch, Project targetProject)
         {
             // If it's on the same project just merge branch
