03ec675b53c409f7a76c64c3abec1ea69130f7c1
Philippe Fortin <philippe.fortin2@ubisoft.com>
2022-02-10 16:01:10 -0500

    change DataMember to JsonPropertyName

--- NGitLab/Models/TimeStats.cs
+++ NGitLab/Models/TimeStats.cs
@@ -1,20 +1,21 @@
 ﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace NGitLab.Models
 {
     [DataContract]
     public class TimeStats
     {
-        [DataMember(Name = "human_time_estimate")]
+        [JsonPropertyName("human_time_estimate")]
         public string HumanTimeEstimate { get; set; }
 
-        [DataMember(Name = "human_total_time_spent")]
+        [JsonPropertyName("human_total_time_spent")]
         public string HumanTotalTimeSpent { get; set; }
 
-        [DataMember(Name = "time_estimate")]
+        [JsonPropertyName("time_estimate")]
         public long TimeEstimateInSec { get; set; }
 
-        [DataMember(Name = "total_time_spent")]
+        [JsonPropertyName("total_time_spent")]
         public long TotalTimeSpentInSec { get; set; }
     }
 }
