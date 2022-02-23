be4bf0f88758c6aa03218d13373e81e41945dc4a
Quentin Metge <quentin.metge@ubisoft.com>
2022-02-23 14:08:10 +0100

    add noteable metadata to note (#192)
    * add noteable metadata to note
    
    * use dynamic enum & specific enum type for noteable type

---
+++ NGitLab/Models/NoteableType.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace NGitLab.Models
+{
+    public enum NoteableType
+    {
+        None = 0,
+        Issue,
+        [EnumMember(Value = "merge_request")]
+        MergeRequest,
+        Snippet,
+        Commit,
+        Epic,
+    }
+}
