5033b64d8059c0e0d3c2191e37a67dd8767182fe
Louis Zanella <louis.zanella@ubisoft.com>
2022-04-09 10:20:43 -0400

    Post-rebase tweaks

--- NGitLab/Models/ReleaseLink.cs
+++ NGitLab/Models/ReleaseLink.cs
@@ -3,7 +3,6 @@ using System.Text.Json.Serialization;
 
 namespace NGitLab.Models
 {
-    [DataContract(Name = "ReleaseLinkType")]
     public enum ReleaseLinkType
     {
         [EnumMember(Value = "other")]
