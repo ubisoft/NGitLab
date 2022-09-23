b3e252721e4aa416a008d23e2a3bc403c02fc44d
Souleymene Rouchou <souleymene.rouchou@ubisoft.com>
2022-09-23 12:37:59 -0400

    add seperate test per review request

--- NGitLab.Mock.Tests/PipelineTests.cs
+++ NGitLab.Mock.Tests/PipelineTests.cs
@@ -15,11 +15,23 @@ namespace NGitLab.Mock.Tests
             var commit = project.Repository.Commit(user, "test");
 
             var pipeline = project.Pipelines.Add(commit.Sha, JobStatus.Success, user);
-
             var job = pipeline.AddNewJob("Test_job", JobStatus.Success);
             job.Trace = "This is a trace\nWith Multiple line";
 
-            var reportSummary = pipeline.TestReportsSummary = new Models.TestReportSummary
+            var client = server.CreateClient();
+            Assert.AreEqual(job.Trace, await client.GetJobs(project.Id).GetTraceAsync(job.Id));
+        }
+
+        [Test]
+        public void Test_pipelines_testreport_summary()
+        {
+            using var server = new GitLabServer();
+            var user = server.Users.AddNew();
+            var project = user.Namespace.Projects.AddNew(project => project.Visibility = Models.VisibilityLevel.Internal);
+            var commit = project.Repository.Commit(user, "test");
+
+            var pipeline = project.Pipelines.Add(commit.Sha, JobStatus.Success, user);
+            pipeline.TestReportsSummary = new Models.TestReportSummary
             {
                 Total = new Models.TestReportSummaryTotals
                 {
@@ -33,8 +45,6 @@ namespace NGitLab.Mock.Tests
             };
 
             var client = server.CreateClient();
-            Assert.AreEqual(job.Trace, await client.GetJobs(project.Id).GetTraceAsync(job.Id));
-
             var summary = client.GetPipelines(project.Id).GetTestReportsSummary(pipeline.Id);
             Assert.AreEqual(60, summary.Total.Time);
             Assert.AreEqual(1157, summary.Total.Count);
