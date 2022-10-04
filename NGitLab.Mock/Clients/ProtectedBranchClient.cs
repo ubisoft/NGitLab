4d8b709bae5b93eac35a5f3fd81e30147bf05e77
Prashanthi Ramesh <prashanthi.ramesh@ubisoft.com>
2022-10-04 12:29:43 -0400

    Mock implementation for GetProtectedBranchClient

---
+++ NGitLab.Mock/Clients/ProtectedBranchClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NGitLab.Models;
+
+namespace NGitLab.Mock.Clients
+{
+    internal class ProtectedBranchClient : ClientBase, IProtectedBranchClient
+    {
+        private readonly int _projectId;
+
+        public ProtectedBranchClient(ClientContext context, int projectId)
+            : base(context)
+        {
+            _projectId = projectId;
+        }
+
+        public ProtectedBranch GetProtectedBranch(string branchName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProtectedBranch[] GetProtectedBranches(string search = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProtectedBranch ProtectBranch(BranchProtect branchProtect)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnprotectBranch(string branchName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
