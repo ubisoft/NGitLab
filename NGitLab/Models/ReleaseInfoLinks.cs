86447dcfecf444ef854a89066788533424f4278c
Louis Zanella <louis.zanella@ubisoft.com>
2022-10-28 15:50:18 -0400

    Add ReleaseInfoLinks

---
+++ NGitLab/Models/ReleaseInfoLinks.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NGitLab.Models
+{
+    public class ReleaseInfoLinks
+    {
+        [JsonPropertyName("self")]
+        public string Self { get; set; }
+
+        [JsonPropertyName("edit_url")]
+        public string EditUrl { get; set; }
+    }
+}
