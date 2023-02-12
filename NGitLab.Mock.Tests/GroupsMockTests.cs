5c9022fb8f9a9c07788a8b9bc19b525beaef66b8
Herbert Yanes <yanessorto93@gmail.com>
2023-02-12 04:50:34 -0500

    Add Subgroups Endpoint to Mock
    - add the subgroups endpoint to the mock
    - add test for the mock

---
+++ NGitLab.Mock.Tests/GroupsMockTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NGitLab.Mock.Config;
+using NUnit.Framework;
+
+namespace NGitLab.Mock.Tests
+{
+    public class GroupsMockTests
+    {
+        [Test]
+        public async Task Test_group_get_by_id()
+        {
+            using var server = new GitLabConfig()
+                .WithUser("user1", isDefault: true)
+                .WithProject("Test")
+                    .WithGroup("G1", 1)
+                    .WithGroup("G2", 2)
+                    .WithGroup("G3", 3)
+                .BuildServer();
+
+            var client = server.CreateClient("user1");
+            var group = await client.Groups.GetByIdAsync(1);
+
+            Assert.AreEqual("G1", group.Name, "Group found is invalid");
+        }
+
+        [Test]
+        public async Task Test_group_get_by_fullpath()
+        {
+            using var server = new GitLabConfig()
+                .WithUser("user1", isDefault: true)
+                .WithProject("Test")
+                    .WithGroup("G1", @namespace: "name1")
+                    .WithGroup("G2", @namespace: "name2")
+                    .WithGroup("G3", @namespace: "name3")
+                .BuildServer();
+
+            var client = server.CreateClient("user1");
+            var group = await client.Groups.GetByFullPathAsync("name3");
+
+            Assert.AreEqual("name3", group.FullPath, "Group found is invalid");
+        }
+
+        [Test]
+        public void Test_get_subgroups_by_id()
+        {
+            using var server = new GitLabConfig()
+                .WithUser("user1", isDefault: true)
+                .WithProject("Test", addDefaultUserAsMaintainer: true)
+                    .WithGroup("parentGroup1", configure: group => group.Id = 12)
+                    .WithGroup("parentGroup2", configure: group => group.Id = 89)
+                    .WithGroup("G1", 2, @namespace: "parentGroup1")
+                    .WithGroup("G2", 3, @namespace: "parentGroup1")
+                    .WithGroup("G3", 4, @namespace: "parentGroup2")
+                .BuildServer();
+
+            var client = server.CreateClient("user1");
+            var group = client.Groups.GetSubgroupsByIdAsync(12, new Models.GroupQuery { });
+
+            Assert.AreEqual(2, group.Count(), "Group found is invalid");
+        }
+
+        [Test]
+        public void Test_get_subgroups_by_fullpath()
+        {
+            using var server = new GitLabConfig()
+               .WithUser("user1", isDefault: true)
+               .WithProject("Test", addDefaultUserAsMaintainer: true)
+                   .WithGroup("parentGroup1", configure: group => group.Id = 12)
+                   .WithGroup("parentGroup2", configure: group => group.Id = 89)
+                   .WithGroup("G1", 2, @namespace: "parentGroup1")
+                   .WithGroup("G2", 3, @namespace: "parentGroup1")
+                   .WithGroup("G3", 4, @namespace: "parentGroup2")
+               .BuildServer();
+
+            var client = server.CreateClient("user1");
+            var group = client.Groups.GetSubgroupsByFullPathAsync("parentgroup1", new Models.GroupQuery { });
+
+            Assert.AreEqual(2, group.Count(), "Group found is invalid");
+        }
+    }
+}
