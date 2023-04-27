e70854e51787315b9e594806c81003aaaaba24af
Thomas Cortes <78750681+Toa741@users.noreply.github.com>
2023-04-27 14:22:46 -0400

    Add page query option for releases (#432)
    - To be able to skip pages if desired (load first results, then next pages if triggered/required)
    - Implement some parts of the query usage in mock

--- NGitLab.Mock/Config/GitLabReleaseInfo.cs
+++ NGitLab.Mock/Config/GitLabReleaseInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace NGitLab.Mock.Config
+﻿using System;
+
+namespace NGitLab.Mock.Config
 {
     /// <summary>
     /// Describes a release in a GitLab project
@@ -12,6 +14,10 @@
         public string Author { get; set; }
 
         public string TagName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ReleasedAt { get; set; }
     }
 
     public class GitLabReleaseInfoCollection : GitLabCollection<GitLabReleaseInfo, GitLabProject>
