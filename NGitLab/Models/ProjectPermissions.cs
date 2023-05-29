49ddaa0c848d4bcffe1617572655d2e8085ddada
Julien Richard <julien.richard@ubisoft.com>
2023-05-29 17:22:40 +0200

    Add project's permissions (#451)
    * Add project's permissions
    
    * Update PublicAPI.Unshipped.txt
    
    * Changes to 6 files in NGitLab

---
+++ NGitLab/Models/ProjectPermissions.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+
+namespace NGitLab.Models
+{
+    public class ProjectPermissions
+    {
+        [JsonPropertyName("project_access")]
+        public ProjectPermission ProjectAccess { get; set; }
+
+        [JsonPropertyName("group_access")]
+        public ProjectPermission GroupAccess { get; set; }
+    }
+}
