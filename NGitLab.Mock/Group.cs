4efebebe3532b6f2522e335aa512f7ccb79b91f5
Thomas Cortes <Thomas.Cortes@ubisoft.com>
2023-06-21 14:13:08 -0400

    Add Project bot creation

--- NGitLab.Mock/Group.cs
+++ NGitLab.Mock/Group.cs
@@ -254,5 +254,22 @@ namespace NGitLab.Mock
                 SharedRunnersMinutesLimit = (int)SharedRunnersLimit.TotalMinutes,
             };
         }
+
+        /// <summary>
+        /// https://docs.gitlab.com/ee/user/group/settings/group_access_tokens.html#bot-users-for-groups
+        /// </summary>
+        /// <param name="accessLevel">AccessLevel to give to the bot user</param>
+        /// <returns>Bot user that have been added to the group</returns>
+        public User CreateBotUser(AccessLevel accessLevel)
+        {
+            var botUsername = $"group_{Id}_bot_{Guid.NewGuid():D}";
+            var bot = new User(botUsername)
+            {
+                Email = $"{botUsername}@noreply.example.com",
+            };
+            Permissions.Add(new Permission(bot, accessLevel));
+            Server.Users.Add(bot);
+            return bot;
+        }
     }
 }
