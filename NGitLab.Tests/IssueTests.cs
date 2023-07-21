575360572e435a5136faa2a531fc3b7ce489a645
Lior Banai <liorbanai@gmail.com>
2023-07-21 19:33:47 +0300

    unit test: change assert to check Empt link (for now, no API to link issues so not links exist but API should not throw) adding create Link should be in diferent PR

--- NGitLab.Tests/IssueTests.cs
+++ NGitLab.Tests/IssueTests.cs
@@ -306,7 +306,9 @@ namespace NGitLab.Tests
             var issue2 = await issuesClient.CreateAsync(new IssueCreate { ProjectId = project.Id, Title = "title2", Description = "related to #1" });
 
             var issues = issuesClient.LinkedTo(project.Id, issue1.IssueId).ToList();
-            Assert.AreEqual(1, issues.Count, "Expected 1. Got {0}", issues.Count);
+
+            // for now, no API to link issues so not links exist but API should not throw
+            Assert.AreEqual(0, issues.Count, "Expected 0. Got {0}", issues.Count);
         }
     }
 }
