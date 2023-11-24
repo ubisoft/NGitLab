f3ab7d0356d18788419728792ad796f763ed769e
Louis Zanella <louis.zanella@ubisoft.com>
2023-11-24 15:12:45 -0500

    Extend Mock to further support so-called LintCI

---
+++ NGitLab.Mock/LintCICollection.cs
@@ -0,0 +1,9 @@
+﻿namespace NGitLab.Mock;
+
+public sealed class LintCICollection : Collection<LintCI>
+{
+    public LintCICollection(GitLabObject container)
+        : base(container)
+    {
+    }
+}
