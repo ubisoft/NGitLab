00c3542b9c2b9ca2f31f56e9e46d8cd2fefb22c0
Mykaël Lemieux-Lafontaine <132388133+mlemieuxlafontaine-ubi@users.noreply.github.com>
2024-02-01 13:17:05 -0500

    Allow MR Comment Range type to be nullable (#625)
    Allow Comment Range type to be nullable

--- NGitLab/Models/Range.cs
+++ NGitLab/Models/Range.cs
@@ -8,5 +8,5 @@ public class Range
     public string LineCode { get; set; }
 
     [JsonPropertyName("type")]
-    public DynamicEnum<RangeType> Type { get; set; }
+    public DynamicEnum<RangeType>? Type { get; set; }
 }
