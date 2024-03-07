71b0472c62fe81045ead736f81adb34dd545a916
John Belcher <jebelche@rockwellautomation.com>
2024-03-07 10:25:42 -0500

    Replace the custom IEnumerator impl with the IEnumerable's enumerator

--- NGitLab/Impl/WebHeadersDictionaryAdaptor.cs
+++ NGitLab/Impl/WebHeadersDictionaryAdaptor.cs
@@ -32,47 +32,16 @@ internal sealed class WebHeadersDictionaryAdaptor : IReadOnlyDictionary<string, 
 
     public bool TryGetValue(string key, out IEnumerable<string> value) => (value = _headers.GetValues(key)) is not null;
 
-    public IEnumerator<KeyValuePair<string, IEnumerable<string>>> GetEnumerator() => new WebHeaderCollectionEnumerator(_headers);
+    public IEnumerator<KeyValuePair<string, IEnumerable<string>>> GetEnumerator() => AsEnumerable().GetEnumerator();
 
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => AsEnumerable().GetEnumerator();
 
     private IEnumerable<KeyValuePair<string, IEnumerable<string>>> AsEnumerable()
     {
-        // FYI: WebHeaderCollection is implemented using arrays, so this will be fast.
-        for (var i = 0; i < _headers.Count; i++)
+        foreach (var key in _headers.AllKeys)
         {
-            yield return new(_headers.GetKey(i), _headers.GetValues(i) ?? Enumerable.Empty<string>());
-        }
-    }
-
-    private class WebHeaderCollectionEnumerator : IEnumerator<KeyValuePair<string, IEnumerable<string>>>
-    {
-        private readonly WebHeaderCollection _headers;
-        private readonly IEnumerator _keyEnumerator;
-
-        public WebHeaderCollectionEnumerator(WebHeaderCollection headers)
-        {
-            _headers = headers ?? throw new ArgumentNullException(nameof(headers));
-            _keyEnumerator = _headers.GetEnumerator();
-        }
-
-        public KeyValuePair<string, IEnumerable<string>> Current
-        {
-            get
-            {
-                var key = (string)_keyEnumerator.Current;
-                return new(key, _headers.GetValues(key) ?? Enumerable.Empty<string>());
-            }
-        }
-
-        object IEnumerator.Current => Current;
-
-        public bool MoveNext() => _keyEnumerator.MoveNext();
-
-        public void Reset() => _keyEnumerator.Reset();
-
-        public void Dispose()
-        {
+            // DO NOT use _headers.GetValues(int index) -- it does not return the expected value.
+            yield return new(key, _headers.GetValues(key) ?? Enumerable.Empty<string>());
         }
     }
 }
