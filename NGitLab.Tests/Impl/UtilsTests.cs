691b6aa4dc206c8d398e402c604160f118bb382f
Louis Zanella <louis.zanella@ubisoft.com>
2024-04-12 09:18:20 -0400

    Add test

---
+++ NGitLab.Tests/Impl/UtilsTests.cs
@@ -0,0 +1,18 @@
+﻿using NGitLab.Models;
+using NUnit.Framework;
+
+namespace NGitLab.Tests.Impl;
+
+public class UtilsTests
+{
+    [TestCase(EventAction.PushedTo, "pushed+to")]
+    [TestCase(EventAction.Accepted, "Accepted")]
+    public void AddParameter_ConsidersEnumMemberAttribute(EventAction value, string expectedQueryParamValue)
+    {
+        const string basePath = "https://gitlab.org/api/v4/stuff";
+        var url = basePath;
+        url = NGitLab.Impl.Utils.AddParameter(url, "event_action", value);
+
+        Assert.That(url, Is.EqualTo($"{basePath}?event_action={expectedQueryParamValue}"));
+    }
+}
