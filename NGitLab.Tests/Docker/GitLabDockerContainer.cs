211acf301facb6362f6a9ee421e9ad04d926511e
Thomas Cortes <78750681+Toa741@users.noreply.github.com>
2024-06-07 15:53:11 -0400

    Resolve GitLab version in GitLabDockerContainer (#695)
    * Resolve GitLab version in GitLabDockerContainer
    
    * Support multiple login inputs on login & token
    
    * Comments

--- NGitLab.Tests/Docker/GitLabDockerContainer.cs
+++ NGitLab.Tests/Docker/GitLabDockerContainer.cs
@@ -15,6 +15,7 @@ using Docker.DotNet;
 using Docker.DotNet.Models;
 using Microsoft.Playwright;
 using NGitLab.Models;
+using NuGet.Versioning;
 using NUnit.Framework;
 using Polly;
 
@@ -25,8 +26,20 @@ public class GitLabDockerContainer
     public const string ContainerName = "NGitLabClientTests";
     public const string ImageName = "gitlab/gitlab-ee";
 
-    // https://hub.docker.com/r/gitlab/gitlab-ee/tags/
-    public const string GitLabDockerVersion = "15.11.9-ee.0"; // Keep in sync with .github/workflows/ci.yml
+    /// <summary>
+    /// GitLab docker image version to spawn.
+    /// Used only on local environment (CI should already have a running GitLab instance from its services)
+    /// </summary>
+    /// <remarks>
+    /// <para>Keep in sync with .github/workflows/ci.yml, use the lowest supported version</para>
+    /// <para>List of available versions: https://hub.docker.com/r/gitlab/gitlab-ee/tags/</para>
+    /// </remarks>
+    private const string LocalGitLabDockerVersion = "15.11.9-ee.0";
+
+    /// <summary>
+    /// Resolved GitLab version taken from the help page once logged in
+    /// </summary>
+    private static string ResolvedGitLabVersion;
 
     private static string s_creationErrorMessage;
     private static readonly SemaphoreSlim s_setupLock = new(initialCount: 1, maxCount: 1);
@@ -91,12 +104,49 @@ public class GitLabDockerContainer
 
     private async Task SetupAsync()
     {
-        await SpawnDockerContainerAsync().ConfigureAwait(false);
-        await LoadCredentialsAsync().ConfigureAwait(false);
-        if (Credentials == null)
+        if (GitLabTestContext.IsContinuousIntegration())
         {
-            await GenerateCredentialsAsync().ConfigureAwait(false);
-            PersistCredentialsAsync();
+            await WaitForCiGitLabInstance().ConfigureAwait(false);
+        }
+        else
+        {
+            await SpawnDockerContainerAsync().ConfigureAwait(false);
+        }
+
+        EnsureChromiumIsInstalled();
+
+        // Use Playwright to launch Chromium
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            // Headless = false,   // Uncomment to have browser window visible
+            // SlowMo = 1000,      // Slows down Playwright operations by the specified amount of ms.
+        });
+        await using var browserContext = await browser.NewContextAsync();
+
+        await LoginAsync(browserContext);
+        await ResolveGitLabVersionAsync(browserContext).ConfigureAwait(false);
+
+        await LoadCredentialsAsync().ConfigureAwait(false);
+
+        if (Credentials != null)
+        {
+            Console.WriteLine("Using credentials from persisted credential file");
+            return;
+        }
+
+        await GenerateCredentialsAsync(browserContext).ConfigureAwait(false);
+        PersistCredentialsAsync();
+
+        static void EnsureChromiumIsInstalled()
+        {
+            TestContext.Progress.WriteLine("Making sure Chromium is installed");
+
+            var exitCode = Microsoft.Playwright.Program.Main(new[] { "install", "--force", "chromium", "--with-deps" });
+            if (exitCode != 0)
+                throw new InvalidOperationException($"Cannot install browser (exit code: {exitCode})");
+
+            TestContext.Progress.WriteLine("Chromium installed");
         }
     }
 
@@ -119,39 +169,8 @@ public class GitLabDockerContainer
 
     private async Task SpawnDockerContainerAsync()
     {
-        // Check if the container is accessible?
-        var isContinuousIntegration = GitLabTestContext.IsContinuousIntegration();
+        Console.WriteLine($"Executing tests locally. Spawning GitLab docker image version '{LocalGitLabDockerVersion}'");
         using var httpClient = new HttpClient();
-        try
-        {
-            Console.WriteLine("Testing " + GitLabUrl);
-            var result = await httpClient.GetStringAsync(GitLabUrl).ConfigureAwait(false);
-            if (isContinuousIntegration) // When not on CI, we want to check the container is on the expected version
-                return;
-        }
-        catch
-        {
-            if (isContinuousIntegration)
-            {
-                var now = Stopwatch.StartNew();
-                while (now.Elapsed < TimeSpan.FromMinutes(10))
-                {
-                    try
-                    {
-                        var result = await httpClient.GetStringAsync(GitLabUrl).ConfigureAwait(false);
-                        return;
-                    }
-                    catch
-                    {
-                    }
-
-                    await Task.Delay(1000);
-                }
-
-                s_creationErrorMessage = "GitLab is not well configured in CI";
-                Assert.Fail(s_creationErrorMessage);
-            }
-        }
 
         // Spawn the container
         // https://docs.gitlab.com/omnibus/settings/configuration.html
@@ -166,7 +185,7 @@ public class GitLabDockerContainer
         {
             TestContext.Progress.WriteLine("Verifying if the GitLab Docker container is using the right image");
             var inspect = await client.Containers.InspectContainerAsync(container.ID).ConfigureAwait(false);
-            var inspectImage = await client.Images.InspectImageAsync(ImageName + ":" + GitLabDockerVersion).ConfigureAwait(false);
+            var inspectImage = await client.Images.InspectImageAsync(ImageName + ":" + LocalGitLabDockerVersion).ConfigureAwait(false);
             if (inspect.Image != inspectImage.ID)
             {
                 TestContext.Progress.WriteLine("Ending GitLab Docker container, as it's using the wrong image");
@@ -179,7 +198,7 @@ public class GitLabDockerContainer
         {
             // Download GitLab images
             TestContext.Progress.WriteLine("Making sure the right GitLab Docker image is available locally");
-            await client.Images.CreateImageAsync(new ImagesCreateParameters { FromImage = ImageName, Tag = GitLabDockerVersion }, new AuthConfig(), new Progress<JSONMessage>()).ConfigureAwait(false);
+            await client.Images.CreateImageAsync(new ImagesCreateParameters { FromImage = ImageName, Tag = LocalGitLabDockerVersion }, new AuthConfig(), new Progress<JSONMessage>()).ConfigureAwait(false);
 
             // Create the container
             TestContext.Progress.WriteLine("Creating the GitLab Docker container");
@@ -194,7 +213,7 @@ public class GitLabDockerContainer
             var response = await client.Containers.CreateContainerAsync(new CreateContainerParameters
             {
                 Hostname = "localhost",
-                Image = ImageName + ":" + GitLabDockerVersion,
+                Image = ImageName + ":" + LocalGitLabDockerVersion,
                 Name = ContainerName,
                 Tty = false,
                 HostConfig = hostConfig,
@@ -264,8 +283,10 @@ public class GitLabDockerContainer
         TestContext.Progress.WriteLine("GitLab Docker container is ready");
     }
 
-    private async Task GenerateCredentialsAsync()
+    private async Task GenerateCredentialsAsync(IBrowserContext browserContext)
     {
+        Console.WriteLine("Requesting credentials from GitLab instance");
+
         var credentials = new GitLabCredential();
         await GenerateAdminToken(credentials).ConfigureAwait(false);
         if (credentials.AdminUserToken != null)
@@ -277,64 +298,48 @@ public class GitLabDockerContainer
 
         async Task GenerateAdminToken(GitLabCredential credentials)
         {
-            EnsureChromiumIsInstalled();
-
-            // Use Playwright to launch Chromium
-            using var playwright = await Playwright.CreateAsync();
-            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                // Headless = false,   // Uncomment to have browser window visible
-                // SlowMo = 1000,      // Slows down Playwright operations by the specified amount of ms.
-            });
-
-            await using var browserContext = await browser.NewContextAsync();
-
-            var page = await browserContext.NewPageAsync();
-            await page.GotoAsync(GitLabUrl.AbsoluteUri);
-
             TestContext.Progress.WriteLine("Generating Credentials");
 
-            var url = await GetCurrentUrl(page);
+            var gitLabVersionAsNuGetVersion = NuGetVersion.Parse(ResolvedGitLabVersion);
+            var isMajorVersion15 = VersionRange.Parse("[15.0,16.0)").Satisfies(gitLabVersionAsNuGetVersion);
+            var isMajorVersion16 = VersionRange.Parse("[16.0,17.0)").Satisfies(gitLabVersionAsNuGetVersion);
 
-            // Login
-            if (url == "/users/sign_in")
+            TestContext.Progress.WriteLine("Creating root token");
+
+            var page = await browserContext.NewPageAsync();
+            await page.GotoAsync(GitLabUrl + "/-/profile/personal_access_tokens");
+
+            var formLocator = page.Locator("main#content-body form");
+
+            var tokenName = "GitLabClientTest-" + DateTime.UtcNow.ToString("yyyyMMdd-HHmmss", CultureInfo.InvariantCulture);
+
+            if (isMajorVersion15)
             {
-                await page.Locator("form#new_user input[name='user[login]']").FillAsync(AdminUserName);
-                await page.Locator("form#new_user input[name='user[password]']").FillAsync(AdminPassword);
-
-                var checkbox = page.Locator("form#new_user input[type=checkbox][name='user[remember_me]']");
-                await checkbox.CheckAsync(new LocatorCheckOptions { Force = true });
-
-                await page.RunAndWaitForResponseAsync(async () =>
-                {
-                    await page.EvalOnSelectorAsync("form#new_user", "form => form.submit()");
-                }, response => response.Status == 200);
-
-                url = await GetCurrentUrl(page);
-            }
-
-            // Create a token
-            if (url == "/")
-            {
-                TestContext.Progress.WriteLine("Creating root token");
-
-                await page.GotoAsync(GitLabUrl + "/-/profile/personal_access_tokens");
-
-                var formLocator = page.Locator("main#content-body form");
-
-                var tokenName = "GitLabClientTest-" + DateTime.UtcNow.ToString("yyyyMMdd-HHmmss", CultureInfo.InvariantCulture);
+                // Try the "old" 15.x.y way
+                formLocator = page.Locator("main#content-body form");
                 await formLocator.GetByLabel("Token name").FillAsync(tokenName);
-
-                foreach (var checkbox in await formLocator.GetByRole(AriaRole.Checkbox).AllAsync())
-                {
-                    await checkbox.CheckAsync(new LocatorCheckOptions { Force = true });
-                }
-
-                await formLocator.GetByRole(AriaRole.Button, new() { Name = "Create personal access token" }).ClickAsync();
-
-                var token = await page.GetByTitle("Copy personal access token").GetAttributeAsync("data-clipboard-text");
-                credentials.AdminUserToken = token;
             }
+            else if (isMajorVersion16)
+            {
+                await page.Locator("main[id='content-body'] button[data-testid='add-new-token-button']").ClickAsync(new LocatorClickOptions { Timeout = 5_000 });
+                formLocator = page.Locator("main[id='content-body'] form[id='js-new-access-token-form']");
+                await formLocator.Locator("input[data-testid='access-token-name-field']").FillAsync(tokenName);
+            }
+            else
+            {
+                s_creationErrorMessage = $"Unable to generate an admin token: resolved GitLab version '{ResolvedGitLabVersion}' doesn't match any supported range in '{nameof(GenerateCredentialsAsync)}'.";
+                Assert.Fail(s_creationErrorMessage);
+            }
+
+            foreach (var checkbox in await formLocator.GetByRole(AriaRole.Checkbox).AllAsync())
+            {
+                await checkbox.CheckAsync(new LocatorCheckOptions { Force = true });
+            }
+
+            await formLocator.GetByRole(AriaRole.Button, new() { Name = "Create personal access token" }).ClickAsync();
+
+            var token = await page.Locator("button[title='Copy personal access token']").GetAttributeAsync("data-clipboard-text");
+            credentials.AdminUserToken = token;
 
             // Get admin login cookie
             // result.Cookie: experimentation_subject_id=XXX; _gitlab_session=XXXX; known_sign_in=XXXX
@@ -349,19 +354,6 @@ public class GitLabDockerContainer
                 }
             }
         }
-
-        static void EnsureChromiumIsInstalled()
-        {
-            TestContext.Progress.WriteLine("Making sure Chromium is installed");
-
-            var exitCode = Microsoft.Playwright.Program.Main(new[] { "install", "--force", "chromium", "--with-deps" });
-            if (exitCode != 0)
-                throw new InvalidOperationException($"Cannot install browser (exit code: {exitCode})");
-
-            TestContext.Progress.WriteLine("Chromium installed");
-        }
-
-        static Task<string> GetCurrentUrl(IPage page) => page.EvaluateAsync<string>("window.location.pathname");
 
         void GenerateUserToken()
         {
@@ -451,4 +443,99 @@ public class GitLabDockerContainer
     {
         return Path.Combine(Path.GetTempPath(), "ngitlab", "credentials.json");
     }
+
+    private async Task WaitForCiGitLabInstance()
+    {
+        Console.WriteLine($"Executing tests on CI. Checking GitLab instance...");
+
+        using var httpClient = new HttpClient();
+        Console.WriteLine("Testing " + GitLabUrl);
+
+        var now = Stopwatch.StartNew();
+        while (now.Elapsed < TimeSpan.FromMinutes(10))
+        {
+            try
+            {
+                var result = await httpClient.GetStringAsync(GitLabUrl).ConfigureAwait(false);
+                return;
+            }
+            catch
+            {
+            }
+
+            await Task.Delay(1000);
+        }
+
+        s_creationErrorMessage = "GitLab is not well configured in CI";
+        Assert.Fail(s_creationErrorMessage);
+    }
+
+    private async Task ResolveGitLabVersionAsync(IBrowserContext browserContext)
+    {
+        Console.WriteLine("Resolving GitLab version from help page...");
+        var page = await browserContext.NewPageAsync();
+        await page.GotoAsync(new Uri(GitLabUrl, "help").AbsoluteUri);
+        var titleLink = await page.QuerySelectorAsync("h1 a");
+
+        if (titleLink is null)
+        {
+            s_creationErrorMessage = "Cannot find title on the help page to get GitLab version";
+            Assert.Fail(s_creationErrorMessage);
+        }
+
+        var version = await titleLink.TextContentAsync();
+
+        if (string.IsNullOrEmpty(version))
+        {
+            s_creationErrorMessage = "Found title on the help page, but the version is empty";
+            Assert.Fail(s_creationErrorMessage);
+        }
+
+        ResolvedGitLabVersion = version.Trim().TrimStart('v');
+        Console.WriteLine($"GitLab resolved version is '{ResolvedGitLabVersion}'");
+    }
+
+    private async Task LoginAsync(IBrowserContext browserContext)
+    {
+        var page = await browserContext.NewPageAsync();
+        await page.GotoAsync(GitLabUrl.AbsoluteUri);
+        var url = await GetCurrentUrl(page);
+
+        if (url != "/users/sign_in")
+        {
+            Console.WriteLine("Already logged in on GitLab instance");
+            return;
+        }
+
+        Console.WriteLine("Logging in on GitLab instance...");
+
+        var v15LoginInput = "form#new_user input[name='user[login]']";
+        var v16LoginInput = "form[data-testid='sign-in-form'] input[name='user[login]']";
+
+        if (await page.QuerySelectorAsync(v15LoginInput) is not null)
+        {
+            await page.Locator(v15LoginInput).FillAsync(AdminUserName);
+            await page.Locator("form#new_user input[name='user[password]']").FillAsync(AdminPassword);
+        }
+        else if (await page.QuerySelectorAsync(v16LoginInput) is not null)
+        {
+            await page.Locator(v16LoginInput).FillAsync(AdminUserName);
+            await page.Locator("form[data-testid='sign-in-form'] input[name='user[password]']").FillAsync(AdminPassword);
+        }
+        else
+        {
+            s_creationErrorMessage = $"Unable to find the correct login input. Please make sure that login form for the GitLab version you target is supported in '{nameof(LoginAsync)}'";
+            Assert.Fail(s_creationErrorMessage);
+        }
+
+        var checkbox = page.Locator("form[data-testid='sign-in-form'] input[type=checkbox][name='user[remember_me]']");
+        await checkbox.CheckAsync(new LocatorCheckOptions { Force = true });
+
+        await page.RunAndWaitForResponseAsync(async () =>
+        {
+            await page.EvalOnSelectorAsync("form[data-testid='sign-in-form']", "form => form.submit()");
+        }, response => response.Status == 200);
+    }
+
+    private static Task<string> GetCurrentUrl(IPage page) => page.EvaluateAsync<string>("window.location.pathname");
 }
