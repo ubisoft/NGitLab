8253448a5e68dd16a577f4ab9e1cc84927fd26d6
phdesroUbi <83839027+phdesroUbi@users.noreply.github.com>
2024-06-18 10:55:22 -0400

    added other type of file for file mocks (#701)
    * added other type of file for file mocks
    
    * change name of file
    
    * removed api remnant

---
+++ NGitLab.Mock/NonTextFile.cs
@@ -0,0 +1,12 @@
+﻿namespace NGitLab.Mock;
+
+public sealed class NonTextFile : File
+{
+    public NonTextFile(string path, byte[] content)
+        : base(path)
+    {
+        Content = content;
+    }
+
+    public override byte[] Content { get; }
+}
