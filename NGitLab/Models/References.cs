aec7a7ac164f1e419893ae426f5d88cfaffd22bb
Aron van Kranenburg <avkranenburg@arkey.systems>
2024-12-05 07:27:52 +0100

    Defined as properties

--- NGitLab/Models/References.cs
+++ NGitLab/Models/References.cs
@@ -5,11 +5,11 @@ namespace NGitLab.Models;
 public class References
 {
     [JsonPropertyName("short")]
-    public string Short;
+    public string Short { get; set; }
 
     [JsonPropertyName("relative")]
-    public string Relative;
+    public string Relative { get; set; }
 
     [JsonPropertyName("full")]
-    public string Full;
+    public string Full { get; set; }
 }
