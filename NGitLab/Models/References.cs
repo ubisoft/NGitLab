dd51622d5b9aa616b0ac1df41200e48eb265bff3
avkranenburg <124564853+avkranenburg@users.noreply.github.com>
2024-12-05 14:45:05 +0100

    Add issue references to Issue model (#806)
    * Add issue references to Issue model
    
    * Defined as properties
    
    * Nuget checks
    
    * Missed the implicit constructor
    
    * fix order of declarations in publicapi

---
+++ NGitLab/Models/References.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NGitLab.Models;
+
+public class References
+{
+    [JsonPropertyName("short")]
+    public string Short { get; set; }
+
+    [JsonPropertyName("relative")]
+    public string Relative { get; set; }
+
+    [JsonPropertyName("full")]
+    public string Full { get; set; }
+}
