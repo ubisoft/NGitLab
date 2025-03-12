0a6c04d87c4349d4375271d2e2da899535c22b71
Jesse Klaasse <jesse.klaasse@indicia.nl>
2025-03-12 16:11:31 +0100

    test: fix tests

--- NGitLab.Tests/ProtectedBranchTests.cs
+++ NGitLab.Tests/ProtectedBranchTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using NGitLab.Models;
 using NGitLab.Tests.Docker;
 using NUnit.Framework;
@@ -8,13 +10,13 @@ namespace NGitLab.Tests;
 public class ProtectedBranchTests
 {
     [Test]
-    [NGitLabRetry]
+    //[NGitLabRetry]
     public async Task ProtectBranch_Test()
     {
         using var context = await GitLabTestContext.CreateAsync();
         var project = context.CreateProject(initializeWithCommits: true);
         var branchClient = context.Client.GetRepository(project.Id).Branches;
-        var branch = branchClient.Create(new BranchCreate { Name = "protectedBranch", Ref = project.DefaultBranch });
+        var branch = branchClient.Create(new BranchCreate { Name = $"protectedBranch-{Guid.NewGuid()}", Ref = project.DefaultBranch });
         var protectedBranchClient = context.Client.GetProtectedBranchClient(project.Id);
         var branchProtect = new BranchProtect(branch.Name)
         {
@@ -36,34 +38,41 @@ public class ProtectedBranchTests
         var existingBranch = protectedBranchClient.GetProtectedBranch(branch.Name);
         ProtectedBranchAndBranchProtectAreEquals(branchProtect, existingBranch);
 
-        // Update protected branch
-        var protectedBranchUpdate = new ProtectedBranchUpdate
-        {
-            AllowForcePush = false,
-            CodeOwnerApprovalRequired = true,
-            AllowedToPush = new[]
-            {
-                new AccessLevelUpdate { Id = existingBranch.PushAccessLevels[0].Id, Destroy = true, }, // This existing one should be removed
-                new AccessLevelUpdate { AccessLevel = AccessLevel.Maintainer, Description = "Maintainer", }, // This one should be added
-            },
-            AllowedToMerge = new[]
-            {
-                new AccessLevelUpdate { Id = existingBranch.MergeAccessLevels[0].Id, AccessLevel = AccessLevel.Maintainer } // The existing one should be updated
-            }
-        };
-        var updatedBranch = protectedBranchClient.UpdateProtectedBranch(branch.Name, protectedBranchUpdate);
-        Assert.That(updatedBranch, Is.Not.Null);
-        Assert.That(updatedBranch.Name, Is.EqualTo(branchProtect.BranchName));
-        Assert.That(updatedBranch.AllowForcePush, Is.EqualTo(protectedBranchUpdate.AllowForcePush));
-        Assert.That(updatedBranch.CodeOwnerApprovalRequired, Is.EqualTo(protectedBranchUpdate.CodeOwnerApprovalRequired));
-        Assert.That(updatedBranch.PushAccessLevels[0].AccessLevel, Is.EqualTo(protectedBranchUpdate.AllowedToPush[1].AccessLevel));
-        Assert.That(updatedBranch.MergeAccessLevels[0].AccessLevel, Is.EqualTo(protectedBranchUpdate.AllowedToMerge[0].AccessLevel));
-
         // Get branches
         Assert.That(protectedBranchClient.GetProtectedBranches(), Is.Not.Empty);
         var protectedBranches = protectedBranchClient.GetProtectedBranches(branch.Name);
         Assert.That(protectedBranches, Is.Not.Empty);
         ProtectedBranchAndBranchProtectAreEquals(branchProtect, protectedBranches[0]);
+
+        // Update protected branch - test delete and update
+        var protectedBranchUpdate = new ProtectedBranchUpdate
+        {
+            AllowForcePush = false,
+            AllowedToPush = new[]
+            {
+                new AccessLevelUpdate { Id = existingBranch.PushAccessLevels.First(l => l.AccessLevel == AccessLevel.Admin).Id, Destroy = true, }, // This existing one should be deleted
+            },
+            AllowedToMerge = new[]
+            {
+                new AccessLevelUpdate { Id = existingBranch.MergeAccessLevels.First(l => l.AccessLevel == AccessLevel.Admin).Id, AccessLevel = AccessLevel.Maintainer } // The existing one should be updated
+            }
+        };
+        var updatedBranch = protectedBranchClient.UpdateProtectedBranch(branch.Name, protectedBranchUpdate);
+        Assert.That(updatedBranch.AllowForcePush, Is.EqualTo(protectedBranchUpdate.AllowForcePush));
+        Assert.That(updatedBranch.PushAccessLevels.Count(l => l.AccessLevel == AccessLevel.Admin), Is.EqualTo(0));
+        Assert.That(updatedBranch.MergeAccessLevels.Count(l => l.AccessLevel == AccessLevel.Admin), Is.EqualTo(0));
+        Assert.That(updatedBranch.MergeAccessLevels.Count(l => l.AccessLevel == AccessLevel.Maintainer), Is.EqualTo(1));
+
+        // Update protected branch - test create
+        protectedBranchUpdate = new ProtectedBranchUpdate
+        {
+            AllowedToPush = new[]
+            {
+                new AccessLevelUpdate { AccessLevel = AccessLevel.Admin }, // This one should be created
+            }
+        };
+        updatedBranch = protectedBranchClient.UpdateProtectedBranch(branch.Name, protectedBranchUpdate);
+        Assert.That(updatedBranch.PushAccessLevels.Count(l => l.AccessLevel == AccessLevel.Admin), Is.EqualTo(1));
 
         // Unprotect branch
         protectedBranchClient.UnprotectBranch(branch.Name);
