dd6ecb5c1c22934d95f6a453147353c5f4470408
Louis Zanella <louis.zanella@ubisoft.com>
2025-05-02 09:04:06 -0400

    Fix Job Artifact data inconsistencies

---
+++ NGitLab.Mock/JobArtifact.cs
@@ -0,0 +1,8 @@
+﻿namespace NGitLab.Mock;
+
+public sealed class JobArtifact : GitLabObject
+{
+    public string Filename { get; set; }
+
+    public long Size { get; set; }
+}
---
+++ NGitLab.Mock/JobArtifactCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace NGitLab.Mock;
+
+public sealed class JobArtifactCollection(GitLabObject container)
+    : Collection<JobArtifact>(container)
+{
+    public override void Add(JobArtifact artifact)
+    {
+        if (artifact is null)
+            throw new ArgumentNullException(nameof(artifact));
+
+        if (string.IsNullOrEmpty(artifact.Filename))
+            throw new ArgumentException("Filename must be set", nameof(artifact));
+
+        if (artifact.Size < 1)
+            throw new ArgumentException("Size must be set", nameof(artifact));
+
+        if (this.Any(g => StringComparer.Ordinal.Equals(g.Filename, artifact.Filename)))
+            throw new NotSupportedException($"Artifact '{artifact.Filename}' already added");
+
+        base.Add(artifact);
+    }
+
+    public new void Clear()
+    {
+        foreach (var item in this)
+        {
+            item.Parent = null;
+        }
+
+        base.Clear();
+    }
+}
