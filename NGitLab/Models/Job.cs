27ce6a2fe28ec6bff3031b2eaf76daaaf39d55f8
theknut <alexander.as.schulz@gmail.com>
2025-06-10 14:02:23 +0200

    feat: add support for job runner manager

--- NGitLab/Models/Job.cs
+++ NGitLab/Models/Job.cs
@@ -10,6 +10,9 @@ public class Job : JobBasic
     [JsonPropertyName("runner")]
     public JobRunner Runner { get; set; }
 
+    [JsonPropertyName("runner_manager")]
+    public JobRunner Runner { get; set; }
+
     [JsonPropertyName("project")]
     public JobProject Project { get; set; }
 
@@ -29,6 +32,39 @@ public class Job : JobBasic
 
         [JsonPropertyName("is_shared")]
         public bool IsShared { get; set; }
+    }
+
+    public class JobRunnerManager
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+    
+        [JsonPropertyName("system_id")]
+        public string SystemId { get; set; }
+    
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+    
+        [JsonPropertyName("revision")]
+        public string Revision { get; set; }
+    
+        [JsonPropertyName("platform")]
+        public string Platform { get; set; }
+    
+        [JsonPropertyName("architecture")]
+        public string Architecture { get; set; }
+    
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+    
+        [JsonPropertyName("contacted_at")]
+        public DateTime ContactedAt { get; set; }
+    
+        [JsonPropertyName("ip_address")]
+        public string IpAddress { get; set; }
+    
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
     }
 
     public class JobArtifact
