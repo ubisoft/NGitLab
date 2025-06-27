f23fbcaf5dd87ef60f98994c19c19db041d788cf
Louis Zanella <louis.zanella@ubisoft.com>
2025-06-27 07:54:10 -0400

    Put back lock

--- NGitLab.Mock/Clients/ClientContext.cs
+++ NGitLab.Mock/Clients/ClientContext.cs
@@ -1,35 +1,40 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace NGitLab.Mock.Clients;
 
-internal sealed class ClientContext
+internal sealed class ClientContext(GitLabServer server, User user)
 {
-    public ClientContext(GitLabServer server, User user)
-    {
-        Server = server;
-        User = user;
-    }
+    private readonly SemaphoreSlim _operationLock = new(1, 1);
 
-    public GitLabServer Server { get; }
+    public GitLabServer Server { get; } = server;
 
-    public User User { get; }
+    public User User { get; } = user;
 
     public bool IsAuthenticated => User != null;
 
     public IDisposable BeginOperationScope()
     {
         Server.RaiseOnClientOperation();
-        return new Releaser();
+        return new Releaser(_operationLock);
     }
 
     private sealed class Releaser : IDisposable
     {
-        public Releaser()
+        private readonly SemaphoreSlim _operationLock;
+
+        public Releaser(SemaphoreSlim operationLock)
         {
+            _operationLock = operationLock;
+            if (Debugger.IsAttached && _operationLock.CurrentCount == 0)
+                Debugger.Break();
+            _operationLock.Wait();
         }
 
         public void Dispose()
         {
+            _operationLock.Release();
         }
     }
 }
