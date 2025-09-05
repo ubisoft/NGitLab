73492d6161be3024b0edb68bb7f788f89ec24fad
Louis Zanella <louis.zanella@ubisoft.com>
2025-09-05 14:18:44 -0400

    Use NeoSmart.AsyncLock in NGitLab.Mock.Clients.ClientContext After a few attempts at fixing the SemaphoreSlim-based ClientContext, use something that is said to be an async/await-friendly replacement for the `lock` implementation. This means it supports reentrancy.

--- NGitLab.Mock/Clients/ClientContext.cs
+++ NGitLab.Mock/Clients/ClientContext.cs
@@ -1,58 +1,21 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Threading;
+using NeoSmart.AsyncLock;
 
 namespace NGitLab.Mock.Clients;
 
 internal sealed class ClientContext(GitLabServer server, User user)
 {
-    private readonly SemaphoreSlim _operationLock = new(1, 1);
+    private readonly AsyncLock _operationLock = new();
 
     public GitLabServer Server { get; } = server;
 
     public User User { get; } = user;
 
-    public bool IsAuthenticated => User != null;
+    public bool IsAuthenticated => User is not null;
 
-    public IDisposable BeginOperationScope(
-        [CallerMemberName] string callingMethod = null,
-        [CallerFilePath] string callingFilePath = null,
-        [CallerLineNumber] int callingLineNumber = -1)
+    public IDisposable BeginOperationScope()
     {
         Server.RaiseOnClientOperation();
-        var releaser = new Releaser(_operationLock);
-
-        // Store caller info for debugging purposes
-        Releaser.MethodWhereLockWasTaken = callingMethod;
-        Releaser.FilePathWhereLockWasTaken = callingFilePath;
-        Releaser.LineNumberWhereLockWasTaken = callingLineNumber;
-
-        return releaser;
-    }
-
-    private sealed class Releaser : IDisposable
-    {
-        private readonly SemaphoreSlim _operationLock;
-
-        public Releaser(SemaphoreSlim operationLock)
-        {
-            _operationLock = operationLock;
-            if (_operationLock.CurrentCount == 0 && Debugger.IsAttached)
-                Debugger.Break();
-            _operationLock.Wait();
-        }
-
-        // The following is for debugging purposes only. It stores info about where the active lock was taken.
-        public static string MethodWhereLockWasTaken { get; set; }
-
-        public static string FilePathWhereLockWasTaken { get; set; }
-
-        public static int LineNumberWhereLockWasTaken { get; set; }
-
-        public void Dispose()
-        {
-            _operationLock.Release();
-        }
+        return _operationLock.Lock();
     }
 }
