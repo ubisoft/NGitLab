f838cb4a3ea6b72702e1598cf56f6334215f10ff
Thomas Cortes <78750681+Toa741@users.noreply.github.com>
2026-03-02 12:10:15 -0500

    feat: implement filename/path for project search in mocks (#1061)
    feat: implement filename/path for project search

---
+++ NGitLab.Mock.Tests/ProjectSearchTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using NGitLab.Mock.Config;
+using NUnit.Framework;
+
+namespace NGitLab.Mock.Tests;
+
+public class ProjectSearchTests
+{
+    [Test]
+    public void ShouldSearchInProjectByFilePath()
+    {
+        using var server = new GitLabConfig()
+            .WithUser("user1", isDefault: true)
+            .WithProject("Test", id: 1)
+            .BuildServer();
+
+        var user = server.Users.Single();
+        var project = server.AllProjects.Single();
+
+        project.Repository.Commit(user, "Initial commit",
+        [
+            File.CreateFromText($"subfolder/README.md", "# Title"),
+        ]);
+
+        var client = server.CreateClient();
+
+        var searchClient = client.GetProjectSearchClient(project.Id);
+        var result = searchClient.GetBlobsAsync(new SearchQuery { Search = "path:subfolder/README.md" });
+
+        var blob = result.Single();
+        Assert.That(blob.FileName, Is.EqualTo("README.md"));
+        Assert.That(blob.Path, Is.EqualTo("subfolder/README.md"));
+    }
+
+    [Test]
+    public void ShouldSearchInProjectByFilename()
+    {
+        using var server = new GitLabConfig()
+            .WithUser("user1", isDefault: true)
+            .WithProject("Test", id: 1)
+            .BuildServer();
+
+        var user = server.Users.Single();
+        var project = server.AllProjects.Single();
+
+        project.Repository.Commit(user, "Initial commit",
+        [
+            File.CreateFromText($"subfolder/README.md", "# Title"),
+        ]);
+
+        var client = server.CreateClient();
+
+        var searchClient = client.GetProjectSearchClient(project.Id);
+        var result = searchClient.GetBlobsAsync(new SearchQuery { Search = "filename:README.md" });
+
+        var blob = result.Single();
+        Assert.That(blob.FileName, Is.EqualTo("README.md"));
+        Assert.That(blob.Path, Is.EqualTo("subfolder/README.md"));
+    }
+}
--- NGitLab.Mock/Clients/ProjectSearchClient.cs
+++ NGitLab.Mock/Clients/ProjectSearchClient.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NGitLab.Extensions;
+using NGitLab.Mock.Internals;
 using NGitLab.Models;
 
 namespace NGitLab.Mock.Clients;
@@ -17,6 +21,62 @@ internal sealed class ProjectSearchClient : ClientBase, ISearchClient
 
     public GitLabCollectionResponse<SearchBlob> GetBlobsAsync(SearchQuery query)
     {
+        if (query.Search.StartsWith("filename:", StringComparison.Ordinal))
+            return SearchByFilename(query);
+
+        if (query.Search.StartsWith("path:", StringComparison.Ordinal))
+            return SearchByPath(query);
+
         throw new NotImplementedException();
+    }
+
+    private GitLabCollectionResponse<SearchBlob> SearchByPath(SearchQuery query)
+    {
+        var path = query.Search["path:".Length..];
+        var project = _context.Server.AllProjects.FindProject(_projectId.ToStringInvariant());
+        var rootUri = new Uri(project.HttpUrl.TrimEnd('/') + '/');
+        var expectedUri = new Uri(rootUri, path);
+        var expectedFilePath = expectedUri.LocalPath;
+
+        if (!System.IO.File.Exists(expectedFilePath))
+            return GitLabCollectionResponse.Create(Array.Empty<SearchBlob>());
+
+        return GitLabCollectionResponse.Create([
+            new SearchBlob
+            {
+                FileName = Path.GetFileName(expectedFilePath),
+                Ref = project.DefaultBranch,
+                ProjectId = project.Id,
+                Path = rootUri.MakeRelativeUri(expectedUri).ToString(),
+                Data = System.IO.File.ReadAllText(expectedFilePath),
+            },
+        ]);
+    }
+
+    private GitLabCollectionResponse<SearchBlob> SearchByFilename(SearchQuery query)
+    {
+        var filename = query.Search["filename:".Length..];
+        var project = _context.Server.AllProjects.FindProject(_projectId.ToStringInvariant());
+        var rootUri = new Uri(project.HttpUrl.TrimEnd('/') + '/');
+        var files = Directory.GetFiles(rootUri.LocalPath, filename, searchOption: SearchOption.AllDirectories);
+
+        if (files.Length == 0)
+            return GitLabCollectionResponse.Create(Array.Empty<SearchBlob>());
+
+        var blobs = new List<SearchBlob>();
+        foreach (var file in files)
+        {
+            var fileUri = new Uri(file);
+            blobs.Add(new SearchBlob
+            {
+                FileName = Path.GetFileName(file),
+                Ref = project.DefaultBranch,
+                ProjectId = project.Id,
+                Path = rootUri.MakeRelativeUri(fileUri).ToString(),
+                Data = System.IO.File.ReadAllText(file),
+            });
+        }
+
+        return GitLabCollectionResponse.Create(blobs);
     }
 }
