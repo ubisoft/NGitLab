5cc8a0c64622e654c4eb0435d324d9b97326af5f
Louis Zanella <louis.zanella@ubisoft.com>
2026-07-30 13:28:07 -0400

    Fix other issues

--- NGitLab.Tests/MergeRequest/MergeRequestCommentsClientTests.cs
+++ NGitLab.Tests/MergeRequest/MergeRequestCommentsClientTests.cs
@@ -56,13 +56,14 @@ public class MergeRequestCommentsClientTests
         Assert.That(editedComment.Body, Is.EqualTo(commentMessageEdit));
         Assert.That(editedComment.CreatedAt, Is.EqualTo(createdAt));
 
-        // Get all
-        var comments = mergeRequestComments.All.ToArray();
+        // 'All' returns all notes, including system notes that GitLab auto-generates (e.g. "changed the merge status to can be merged" or similar state-change events).
+        // Get all non-system notes
+        var comments = mergeRequestComments.All.Where(c => !c.System).ToArray();
         Assert.That(comments, Is.Not.Empty);
 
         // Delete
         mergeRequestComments.Delete(comment.Id);
-        comments = mergeRequestComments.All.ToArray();
+        comments = mergeRequestComments.All.Where(c => !c.System).ToArray();
         Assert.That(comments, Is.Empty);
     }
 
